ab79861e110b2194cb05a410667e20acaf8b2570
ZOZAKIEWICZ AARON <aj.zozakiewicz@gmail.com>
2018-04-23 14:44:17 -0500

    tweaks to UI

--- components/App.js
+++ components/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { Grid, Segment } from 'semantic-ui-react'
+import { Grid, Segment, Container, Header } from 'semantic-ui-react'
 import ExampleCard from './Card'
 
 class App extends React.Component {
   render() {
     return (
-      <Grid columns='equal' container>
-        <Grid.Column>
-          <ExampleCard />
-        </Grid.Column>
-      </Grid>
+      <Container>
+        <Header as='h1'>Game of Thrones React App</Header>
+        <Grid columns='equal'>
+          <Grid.Column>
+            <ExampleCard />
+          </Grid.Column>
+        </Grid>
+      </Container>
     )
   }
 }
